f7153c873123889d82c68c9615185f5bec50a35c
Niels <info@heimseiten.de>
2022-04-27 13:57:15 +0200

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -3,8 +3,8 @@
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\DataContainer;
 
-$GLOBALS['TL_DCA']['tl_settings']['fields']['newSiteVisible'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_settings']['newSiteVisible'],
+$GLOBALS['TL_DCA']['tl_settings']['fields']['publishPageOnCreate'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate'],
     'inputType' => 'checkbox', 
     'eval'      => array('tl_class' => 'w50 m12'),
     'sql'       => "char(1) NOT NULL default ''" 
@@ -12,7 +12,7 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['newSiteVisible'] = [
 
 PaletteManipulator::create()
     ->addLegend('CustomBackendSettings', 'backend_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('newSiteVisible', 'CustomBackendSettings', PaletteManipulator::POSITION_APPEND)
+    ->addField('publishPageOnCreate', 'CustomBackendSettings', PaletteManipulator::POSITION_APPEND)
     
     ->applyToPalette('default', 'tl_settings') 
 ;
