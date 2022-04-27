06f6f3e10eaa5cf28b51ffda727792d27c122a08
Niels <info@heimseiten.de>
2022-04-27 14:08:10 +0200

    added language

---
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate']   = array('Artikel beim Neuerstellen direkt veröffentlichen', '');
+$GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate']   = array('Seite beim Neuerstellen direkt veröffentlichen', '');
---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate']   = array('publish article on create', '');
+$GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate']   = array('publish pagel on create', '');
