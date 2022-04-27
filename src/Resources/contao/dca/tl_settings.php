168e2da10ea1123ff955b26302e632ff70f0dcdb
Niels <info@heimseiten.de>
2022-04-27 17:56:03 +0200

    removed class m12

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -6,56 +6,56 @@ use Contao\DataContainer;
 $GLOBALS['TL_DCA']['tl_settings']['fields']['publishPageOnCreate'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''" 
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['publishArticleOnCreate'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''" 
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['highlightFilteredPageTree'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['highlightFilteredPageTree'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''"
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['highlightSearchedPageTree'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['highlightSearchedPageTree'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''"
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['enlargeCssField'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['enlargeCssField'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''"
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['copiedPageOrArticleWithoutLabelCopy'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['copiedPageOrArticleWithoutLabelCopy'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''"
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['copyNewsWithAllDetails'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['copyNewsWithAllDetails'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''"
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['copyEventsWithAllDetails'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['copyEventsWithAllDetails'],
     'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default ''"
 ];
 
