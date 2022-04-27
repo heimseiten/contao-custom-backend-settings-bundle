e2193f1782a2a6979325d3ebaea92eac979e40bc
Niels <info@heimseiten.de>
2022-04-27 12:19:37 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to Contao-Custom-Backend-Settings
+This bundle is still under construction.
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+if (TL_MODE == 'BE') {
+    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/contao-custom-backend-settings.scss|static';
+}
