0c18d9308e53ab86ade175a681a37678460e2f98
Niels <info@heimseiten.de>
2022-04-27 14:11:56 +0200

    changed language

--- src/Resources/contao/languages/en/tl_settings.php
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettings'] = array('Custom backend settings', '');
+$GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettings'] = 'Custom backend settings';
 
 $GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate'] = array('publish article on create', '');
 $GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate'] = array('publish pagel on create', '');
