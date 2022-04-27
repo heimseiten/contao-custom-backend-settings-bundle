59180ae1b2c0b5205735f0ffa1e173ea5afa4151
Niels <info@heimseiten.de>
2022-04-27 14:10:27 +0200

    added language

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -1,4 +1,6 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettings']   = array('erweiterte Backend-Einstellungen', '');
+
 $GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate']   = array('Artikel beim Neuerstellen direkt veröffentlichen', '');
 $GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate']   = array('Seite beim Neuerstellen direkt veröffentlichen', '');
