2c08c370cbabed3ac1090c4a7ddcd0f9ca9c0ca6
Niels <info@heimseiten.de>
2022-08-18 13:11:37 +0200

    changed language

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -2,18 +2,18 @@
 
 $GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettings']   = 'erweiterte Backend-Einstellungen';
 
-$GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate']   = array('Artikel beim Neuerstellen direkt veröffentlichen', '');
-$GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate']   = array('Seite beim Neuerstellen direkt veröffentlichen', '');
-$GLOBALS['TL_LANG']['tl_settings']['publishNewsOnCreate']   = array('Nachricht beim Neuerstellen direkt veröffentlichen', '');
-$GLOBALS['TL_LANG']['tl_settings']['publishEventOnCreate']   = array('Event beim Neuerstellen direkt veröffentlichen', '');
+$GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate']   = array('Artikel direkt veröffentlichen', 'Artikel beim Neuerstellen direkt veröffentlichen');
+$GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate']   = array('Seite direkt veröffentlichen', 'Seite beim Neuerstellen direkt veröffentlichen');
+$GLOBALS['TL_LANG']['tl_settings']['publishNewsOnCreate']   = array('Nachricht direkt veröffentlichen', 'Nachricht beim Neuerstellen direkt veröffentlichen');
+$GLOBALS['TL_LANG']['tl_settings']['publishEventOnCreate']   = array('Event direkt veröffentlichen', 'Event beim Neuerstellen direkt veröffentlichen');
 $GLOBALS['TL_LANG']['tl_settings']['highlightFilteredPageTree'] = array('gefilterten Seitenbaum hervorheben', '');
 $GLOBALS['TL_LANG']['tl_settings']['highlightSearchedPageTree'] = array('durchsuchten Seitenbaum hervorheben', '');
 $GLOBALS['TL_LANG']['tl_settings']['enlargeCssField'] = array('CSS Eingabefeld vergrößern', '');
-$GLOBALS['TL_LANG']['tl_settings']['copiedPageOrArticleWithoutLabelCopy'] = array('beim Kopieren von Seiten oder Artikeln nicht "(Kopie)" an den Namen hängen', '');
-$GLOBALS['TL_LANG']['tl_settings']['copyEventsWithAllDetails'] = array('Events mit allen Details, wie bspw. Datum, Veröffentlichungsstatus usw., kopieren', '');
-$GLOBALS['TL_LANG']['tl_settings']['copyNewsWithAllDetails'] = array('Nachrichten mit allen Details, wie bspw. Datum, Veröffentlichungsstatus usw., kopieren', '');
+$GLOBALS['TL_LANG']['tl_settings']['copiedPageOrArticleWithoutLabelCopy'] = array('Kopieren ohne "(Kopie)" anzuhängen', 'beim Kopieren von Seiten oder Artikeln nicht "(Kopie)" an den Namen hängen');
+$GLOBALS['TL_LANG']['tl_settings']['copyEventsWithAllDetails'] = array('echtes kopieren von Events', 'Events mit allen Details, wie bspw. Datum, Veröffentlichungsstatus usw., kopieren');
+$GLOBALS['TL_LANG']['tl_settings']['copyNewsWithAllDetails'] = array('echtes kopieren von News', 'Nachrichten mit allen Details, wie bspw. Datum, Veröffentlichungsstatus usw., kopieren');
 $GLOBALS['TL_LANG']['tl_settings']['formFieldAllowHtml'] = array('HTML in Formularfeldern erlauben', '');
-$GLOBALS['TL_LANG']['tl_settings']['memberFieldsNotMandatory'] = array('Vorname, Nachname und Land in den Mitgliederdaten nicht verpflichtent', '');
+$GLOBALS['TL_LANG']['tl_settings']['memberFieldsNotMandatory'] = array('Vorname, Nachname & Land nicht Pflichtfelder', 'Vorname, Nachname und Land in den Mitgliederdaten nicht verpflichtent');
 $GLOBALS['TL_LANG']['tl_settings']['readMoreLabelDe'] = array('deutsche Beschriftung für den "Weiterlesen"-Link', '');
 $GLOBALS['TL_LANG']['tl_settings']['readMoreLabelEn'] = array('englische Beschriftung für den "Weiterlesen"-Link', '');
 $GLOBALS['TL_LANG']['tl_settings']['previousLabelDe'] = array('englische Beschriftung für den "Zurück"-Link', '');
