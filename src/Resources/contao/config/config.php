c95a04a38b0151a77adf24a5108bdaa6dba9ef2d
Niels <info@heimseiten.de>
2022-08-19 21:08:46 +0200

    preparation for Contao 5

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,17 +5,17 @@ use Symfony\Component\HttpFoundation\Request;
 
 if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
 
-    if ( \Config::get('highlightFilteredPageTree') ) {
+    //if ( \Config::get('highlightFilteredPageTree') ) {
         $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs_highlight_filtered_page_tree.scss|static';
-    }
+    //}
 
-    if ( \Config::get('highlightSearchedPageTree') ) {
+    //if ( \Config::get('highlightSearchedPageTree') ) {
         $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs_highlight_searched_page_tree.scss|static';
-    }
+    //}
 
-    if ( \Config::get('enlargeCssField') ) {
+    //if ( \Config::get('enlargeCssField') ) {
         $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs_enlarge_css_field.scss|static';
-    }
+    //}
     
     $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs.scss|static';
 
