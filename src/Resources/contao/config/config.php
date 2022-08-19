971ef63c5811d8fa57df9e1a3e9c02c6044ca282
Niels <info@heimseiten.de>
2022-08-19 14:51:28 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,9 @@
 <?php
 
-if (TL_MODE == 'BE') {
+use Contao\System;
+use Symfony\Component\HttpFoundation\Request;
+
+if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
 
     if ( \Config::get('highlightFilteredPageTree') ) {
         $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs_highlight_filtered_page_tree.scss|static';
