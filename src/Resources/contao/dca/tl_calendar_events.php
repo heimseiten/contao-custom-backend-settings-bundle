4a0bb7de38dc0b375616ffd8736a924561f381a9
Niels <info@heimseiten.de>
2023-03-25 11:20:44 +0100

    Update tl_calendar_events.php

--- src/Resources/contao/dca/tl_calendar_events.php
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -1,14 +1,10 @@
 <?php
 
-if ( \Config::get('copyEventsWithAllDetails') ) {
     $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['startTime']['eval']['doNotCopy'] = false;
     $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['endTime']['eval']['doNotCopy'] = false;
     $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['startDate']['eval']['doNotCopy'] = false;
     $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['endDate']['eval']['doNotCopy'] = false;
     $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['addTime']['eval']['doNotCopy'] = false;  
     $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['published']['eval']['doNotCopy'] = false;
-}
 
-if ( \Config::get('publishEventOnCreate') ) {
     $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['published']['default'] = true;
-}
