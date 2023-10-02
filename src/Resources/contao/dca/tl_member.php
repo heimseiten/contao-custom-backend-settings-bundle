2f97027c4cfcfd61fb07f3a1a75f47e3b1fc6e3f
Niels <info@heimseiten.de>
2023-10-02 12:57:10 +0200

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -1,5 +1,7 @@
 <?php
 
+if ( Contao\Config::get('memberFieldsNotMandatory') ) {
     $GLOBALS['TL_DCA']['tl_member']['fields']['firstname']['eval']['mandatory'] = false;
     $GLOBALS['TL_DCA']['tl_member']['fields']['lastname']['eval']['mandatory'] = false;
     $GLOBALS['TL_DCA']['tl_member']['fields']['country']['eval']['mandatory'] = false;
+}
