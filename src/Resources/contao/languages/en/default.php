0e3b1b8e5d5c2d2787eef74215eaa343db4f166b
Niels <info@heimseiten.de>
2023-10-02 13:02:52 +0200

    Update default.php

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -4,5 +4,19 @@ use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 
 if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
+    if ( Contao\Config::get('copiedPageOrArticleWithoutLabelCopy') ) {
         $GLOBALS['TL_LANG']['MSC']['copyOf'] = '%s';
+    }
+}
+
+if ( Contao\Config::get('readMoreLabelEn') ) {
+    $GLOBALS['TL_LANG']['MSC']['more']  = Contao\Config::get('readMoreLabelEn');
+}
+
+if ( Contao\Config::get('previousLabelEn') ) {    
+    $GLOBALS['TL_LANG']['MSC']['previous'] = Contao\Config::get('previousLabelEn');
+}
+
+if ( Contao\Config::get('nextLabelEn') ) {    
+    $GLOBALS['TL_LANG']['MSC']['next'] = Contao\Config::get('nextLabelEn');
 }
