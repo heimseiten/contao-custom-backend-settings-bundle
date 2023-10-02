4cb69ed0a6ccddf3ed2d71bd6f0d9ff76de8627c
Niels <info@heimseiten.de>
2023-10-02 13:18:19 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,15 +5,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
 
-        if ( \Config::get('highlightFilteredPageTree') ) {
+        if ( Contao\Config::get('highlightFilteredPageTree') ) {
                 $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs_highlight_filtered_page_tree.scss|static';
         }
 
-        if ( \Config::get('highlightSearchedPageTree') ) {
+        if ( Contao\Config::get('highlightSearchedPageTree') ) {
                 $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs_highlight_searched_page_tree.scss|static';
         }
 
-        if ( \Config::get('enlargeCssField') ) {
+        if ( Contao\Config::get('enlargeCssField') ) {
                 $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaocustombackendsettings/cbs_enlarge_css_field.scss|static';
         }
     
