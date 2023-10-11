2b170155865496bc96bf5ddfb96f686955b0824d
Niels <info@heimseiten.de>
2023-10-11 12:14:27 +0200

    Update tl_settings.php

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -1,11 +1,16 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettings']   = 'erweiterte Backend-Einstellungen';
+$GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettingsLabels']   = 'erweiterte Backend-Einstellungen - Beschriftungen Frontend';
+$GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettingsAppearance']   = 'erweiterte Backend-Einstellungen - Erscheinungsbild Backend';
 
 $GLOBALS['TL_LANG']['tl_settings']['publishArticleOnCreate']   = array('Artikel direkt veröffentlichen', 'Artikel beim Neuerstellen direkt veröffentlichen');
 $GLOBALS['TL_LANG']['tl_settings']['publishPageOnCreate']   = array('Seite direkt veröffentlichen', 'Seite beim Neuerstellen direkt veröffentlichen');
 $GLOBALS['TL_LANG']['tl_settings']['publishNewsOnCreate']   = array('Nachricht direkt veröffentlichen', 'Nachricht beim Neuerstellen direkt veröffentlichen');
 $GLOBALS['TL_LANG']['tl_settings']['publishEventOnCreate']   = array('Event direkt veröffentlichen', 'Event beim Neuerstellen direkt veröffentlichen');
+$GLOBALS['TL_LANG']['tl_settings']['formFieldAllowHtml'] = array('HTML in Formularfeldern erlauben', 'Um bspw. einen Zeilenumbruch mit &lt;br&gt; in der Feldbezeichnung einzufügen.');
+
+
 $GLOBALS['TL_LANG']['tl_settings']['highlightFilteredPageTree'] = array('gefilterten Seitenbaum hervorheben', '');
 $GLOBALS['TL_LANG']['tl_settings']['highlightSearchedPageTree'] = array('durchsuchten Seitenbaum hervorheben', '');
 $GLOBALS['TL_LANG']['tl_settings']['enlargeCssField'] = array('Feld "CSS-Klasse" verlängern', 'Verlängert das Feld "CSS-Klasse" in den Einstellungen der Artikel und der Inhaltselemente, so dass es auch nach dem eintragen von mehreren Klassen noch übersichtlich ist.');
@@ -15,14 +20,11 @@ $GLOBALS['TL_LANG']['tl_settings']['copiedPageOrArticleWithoutLabelCopy'] = arra
 $GLOBALS['TL_LANG']['tl_settings']['copyEventsWithAllDetails'] = array('echtes Kopieren von Events', 'Events mit allen Details, wie bspw. Datum, Veröffentlichungsstatus usw., kopieren');
 $GLOBALS['TL_LANG']['tl_settings']['copyNewsWithAllDetails'] = array('echtes Kopieren von News', 'Nachrichten mit allen Details, wie bspw. Datum, Veröffentlichungsstatus usw., kopieren');
 $GLOBALS['TL_LANG']['tl_settings']['disableButtonAfterClick'] = array('Schaltflächen nach dem Klicken ausblenden', '"Speichern", "Speichern und schließen", usw. werden nach einem Klick ausgeblendet.');
-$GLOBALS['TL_LANG']['tl_settings']['formFieldAllowHtml'] = array('HTML in Formularfeldern erlauben', 'Um bspw. einen Zeilenumbruch mit &lt;br&gt; in der Feldbezeichnung einzufügen.');
 $GLOBALS['TL_LANG']['tl_settings']['memberFieldsNotMandatory'] = array('Vorname, Nachname & Land nicht Pflichtfelder', 'Vorname, Nachname und Land in den Mitgliederdaten nicht verpflichtent');
 
 $GLOBALS['TL_LANG']['tl_settings']['readMoreLabelDe'] = array('"Weiterlesen"-Link - deutsche Beschriftung', '');
 $GLOBALS['TL_LANG']['tl_settings']['readMoreLabelEn'] = array('"Weiterlesen"-Link - englische Beschriftung', '');
-
 $GLOBALS['TL_LANG']['tl_settings']['previousLabelDe'] = array('"Zurück"-Link - deutsche Beschriftung', '');
 $GLOBALS['TL_LANG']['tl_settings']['nextLabelDe'] = array('"Vorwärts"-Link - deutsche Beschriftung', '');
-
 $GLOBALS['TL_LANG']['tl_settings']['previousLabelEn'] = array('Zurück"-Link - englische Beschriftung', '');
 $GLOBALS['TL_LANG']['tl_settings']['nextLabelEn'] = array('"Vorwärts"-Link - englische Beschriftung', '');
