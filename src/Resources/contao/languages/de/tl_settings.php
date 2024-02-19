064b79694f0998dc6686a8d6772be5f36ef96d9b
Niels <info@heimseiten.de>
2024-02-19 13:13:20 +0100

    Update tl_settings.php

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -22,16 +22,13 @@ $GLOBALS['TL_LANG']['tl_settings']['copyNewsWithAllDetails'] = array('echtes Kop
 $GLOBALS['TL_LANG']['tl_settings']['disableButtonAfterClick'] = array('Schaltflächen nach dem Klicken ausblenden', '"Speichern", "Speichern und schließen", usw. werden nach einem Klick ausgeblendet.');
 $GLOBALS['TL_LANG']['tl_settings']['memberFieldsNotMandatory'] = array('Vorname, Nachname & Land nicht Pflichtfelder', 'Vorname, Nachname und Land in den Mitgliederdaten nicht verpflichtent');
 
+$GLOBALS['TL_LANG']['tl_settings']['limitBackendWidth'] = array('Breite des Backends begrenzen', 'Begrenzt die Breite des Beckends auf 1920 Pixel.');
+$GLOBALS['TL_LANG']['tl_settings']['enlargePreviewImagesInFileManager'] = array('Vorschaubilder in der Dateiverwaltung vergrößern', 'Auf 250 Pixel Breite.');
+$GLOBALS['TL_LANG']['tl_settings']['enlargeFontSizeWhenViewportIsMoreThan1920px'] = array('Schrift des Backends vergrößern', 'Wenn das Fenster breiter als 1920 Pixel ist.');
+
 $GLOBALS['TL_LANG']['tl_settings']['readMoreLabelDe'] = array('"Weiterlesen"-Link - deutsche Beschriftung', '');
 $GLOBALS['TL_LANG']['tl_settings']['readMoreLabelEn'] = array('"Weiterlesen"-Link - englische Beschriftung', '');
-
-$GLOBALS['TL_LANG']['tl_settings']['previousLabelDe'] = array('Seitennummerierung - "Zurück"-Link - deutsche Beschriftung', '');
-$GLOBALS['TL_LANG']['tl_settings']['previousLabelEn'] = array('Seitennummerierung - "Zurück"-Link - englische Beschriftung', '');
-
-$GLOBALS['TL_LANG']['tl_settings']['nextLabelDe'] = array('Seitennummerierung - "Vorwärts"-Link - deutsche Beschriftung', '');
-$GLOBALS['TL_LANG']['tl_settings']['nextLabelEn'] = array('Seitennummerierung - "Vorwärts"-Link - englische Beschriftung', '');
-
-$GLOBALS['TL_LANG']['tl_settings']['firstLabelDe'] = array('Seitennummerierung - "Anfang"-Link - deutsche Beschriftung', '');
-$GLOBALS['TL_LANG']['tl_settings']['lastLabelDe'] = array('Seitennummerierung - "Ende"-Link - deutsche Beschriftung', '');
-
-$GLOBALS['TL_LANG']['tl_settings']['newsOverviewDe'] = array('"Zurück zur Übersicht"-Link - deutsche Beschriftung', '');
+$GLOBALS['TL_LANG']['tl_settings']['previousLabelDe'] = array('"Zurück"-Link - deutsche Beschriftung', '');
+$GLOBALS['TL_LANG']['tl_settings']['nextLabelDe'] = array('"Vorwärts"-Link - deutsche Beschriftung', '');
+$GLOBALS['TL_LANG']['tl_settings']['previousLabelEn'] = array('Zurück"-Link - englische Beschriftung', '');
+$GLOBALS['TL_LANG']['tl_settings']['nextLabelEn'] = array('"Vorwärts"-Link - englische Beschriftung', '');
