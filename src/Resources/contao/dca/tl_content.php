c3fdc44d3c98ca39bc29fd13ee3d5e3d5b7fa337
Niels <info@heimseiten.de>
2024-04-25 14:18:24 +0200

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -3,3 +3,7 @@
 if ( Contao\Config::get('addParagraphOptionToHeadlineField') ) {
     array_push( $GLOBALS['TL_DCA']['tl_content']['fields']['headline']['options'], 'p' );
 }
+
+if ( Contao\Config::get('addStrongOptionToHeadlineField') ) {
+    array_push( $GLOBALS['TL_DCA']['tl_content']['fields']['headline']['options'], 'strong' );
+}
