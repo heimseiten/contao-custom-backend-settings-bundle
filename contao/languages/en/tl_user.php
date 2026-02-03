d82e539d8dca6b2f6f0895b28494dccd244eacdb
Niels <info@heimseiten.de>
2026-02-03 17:33:13 +0100

    Update backend SCSS file reference in tl_user.php

--- contao/languages/en/tl_user.php
+++ contao/languages/en/tl_user.php
@@ -7,4 +7,4 @@ $GLOBALS['TL_LANG']['tl_settings']['highlightSearchedPageTree'] = array('highlig
 $GLOBALS['TL_LANG']['tl_settings']['enlargeCssField'] = array('enlarge css field', '');
 $GLOBALS['TL_LANG']['tl_settings']['enlargePreviewImagesInFileManager'] = array('Enlarge preview images in file manager', 'To 250 pixels width.');
 $GLOBALS['TL_LANG']['tl_user']['disableLinksInPageTreeToFilterTree'] = array('Deactivate links to filter in the page tree and in files', 'Deactivates the clickability of the individual page names to filter the page tree.');
-$GLOBALS['TL_LANG']['tl_user']['loadBackendSCSS'] = array('load your own SCSS file for the backend', 'Loads the file backend.scss from the directory /files/layout/css/.');
+$GLOBALS['TL_LANG']['tl_user']['loadBackendSCSS'] = array('load your own SCSS file for the backend', 'Loads the file backend.css from the directory /files/layout/css/.');
