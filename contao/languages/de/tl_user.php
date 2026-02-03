4687a781cc7e2f90c8b11fd2280c4acf3153b1af
Niels <info@heimseiten.de>
2026-02-03 17:33:31 +0100

    Update SCSS file reference to backend.css

--- contao/languages/de/tl_user.php
+++ contao/languages/de/tl_user.php
@@ -13,4 +13,4 @@ $GLOBALS['TL_LANG']['tl_user']['copyNewsWithAllDetails'] = array('echtes Kopiere
 $GLOBALS['TL_LANG']['tl_user']['memberFieldsNotMandatory'] = array('Vorname, Nachname & Land nicht Pflichtfelder', 'Vorname, Nachname und Land in den Mitgliederdaten nicht verpflichtent');
 $GLOBALS['TL_LANG']['tl_user']['enlargePreviewImagesInFileManager'] = array('Vorschaubilder in der Dateiverwaltung vergrößern', 'Auf 250 Pixel Breite.');
 $GLOBALS['TL_LANG']['tl_user']['disableLinksInPageTreeToFilterTree'] = array('Links zum Filtern im Seitenbaum und in Dateien deaktivieren', 'Deaktiviert die Klickbarkeit der einzelnen Seitennamen um den Seitenbaum zu filtern.');
-$GLOBALS['TL_LANG']['tl_user']['loadBackendSCSS'] = array('eigene SCSS Datei für das Backend laden', 'Lädt die Datei backend.scss aus dem Verzeichnis /files/layout/css/.');
+$GLOBALS['TL_LANG']['tl_user']['loadBackendSCSS'] = array('eigene SCSS Datei für das Backend laden', 'Lädt die Datei backend.css aus dem Verzeichnis /files/layout/css/.');
