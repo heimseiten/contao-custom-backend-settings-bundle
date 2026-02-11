8f6be9c3fbd4250814c2c7abdc7e7fe90cb05de6
Niels <info@heimseiten.de>
2026-02-11 10:36:50 +0100

    Add focus and submit functionality on load

--- public/focus_the_field_name_on_load.js
+++ public/focus_the_field_name_on_load.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    if (document.querySelector("#ctrl_title")) {
-        document.querySelector("#ctrl_title").focus()
-    }
+        if (document.querySelector("#ctrl_title")) {
+            document.querySelector("#ctrl_title").focus()
+        }
+        var btn = document.querySelector('button:has(img[alt="Oben einfügen"])')
+        if (btn) {
+          btn.closest("form").submit()
+        } 
 })
