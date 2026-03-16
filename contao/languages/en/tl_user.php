dd2bd5fe673842be5cd59e56bfd1bc469592a4e7
Niels <info@heimseiten.de>
2026-03-16 18:18:21 +0100

    Update tl_user.php

--- contao/languages/en/tl_user.php
+++ contao/languages/en/tl_user.php
@@ -1,10 +1,17 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_settings']['CustomBackendSettings'] = 'Custom backend settings';
+$GLOBALS['TL_LANG']['tl_user']['CustomBackendSettingsAppearance'] = 'Custom backend settings';
 
-$GLOBALS['TL_LANG']['tl_settings']['highlightFilteredPageTree'] = array('highlight filtered page tree', '');
-$GLOBALS['TL_LANG']['tl_settings']['highlightSearchedPageTree'] = array('highlight searched page tree', '');
-$GLOBALS['TL_LANG']['tl_settings']['enlargeCssField'] = array('enlarge css field', '');
-$GLOBALS['TL_LANG']['tl_settings']['enlargePreviewImagesInFileManager'] = array('Enlarge preview images in file manager', 'To 250 pixels width.');
+$GLOBALS['TL_LANG']['tl_user']['highlightFilteredPageTree'] = array('highlight filtered page tree', '');
+$GLOBALS['TL_LANG']['tl_user']['highlightSearchedPageTree'] = array('highlight searched page tree', '');
+$GLOBALS['TL_LANG']['tl_user']['enlargeCssField'] = array('enlarge css field', '');
+$GLOBALS['TL_LANG']['tl_user']['enlargeOptionField'] = array('enlarge option field', 'Enlarges the options field, e.g. in the select menu in the forms.');
+$GLOBALS['TL_LANG']['tl_user']['enlargeTableFields'] = array('enlarge fields of content element "table"', '');
+$GLOBALS['TL_LANG']['tl_user']['copiedPageOrArticleWithoutLabelCopy'] = array('copy without appending "(copy)"', 'when copying pages or articles do not append "(copy)" to the name');
+$GLOBALS['TL_LANG']['tl_user']['copyEventsWithAllDetails'] = array('real copying of events', 'copy events with all details, such as date, publication status, etc.');
+$GLOBALS['TL_LANG']['tl_user']['copyNewsWithAllDetails'] = array('real copying of news', 'copy news with all details, such as date, publication status, etc.');
+$GLOBALS['TL_LANG']['tl_user']['memberFieldsNotMandatory'] = array('first name, last name & country not mandatory fields', 'first name, last name and country in the member data not mandatory');
+$GLOBALS['TL_LANG']['tl_user']['enlargePreviewImagesInFileManager'] = array('Enlarge preview images in file manager', 'To 250 pixels width.');
 $GLOBALS['TL_LANG']['tl_user']['disableLinksInPageTreeToFilterTree'] = array('Deactivate links to filter in the page tree and in files', 'Deactivates the clickability of the individual page names to filter the page tree.');
 $GLOBALS['TL_LANG']['tl_user']['loadBackendSCSS'] = array('load your own SCSS file for the backend', 'Loads the file backend.css from the directory /files/layout/css/.');
+$GLOBALS['TL_LANG']['tl_user']['viewIdAndCssInArticlesAndContentElements'] = array('Show ID and CSS in articles and content elements', 'Shows the ID and the CSS classes in the backend labels of the articles and content elements.');
