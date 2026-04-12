7db50a413f0d1da45022c7991b71455dd5213721
Niels <info@heimseiten.de>
2026-04-12 22:11:03 +0200

    Add loadExtension method to bundle class

--- src/HeimseitenContaoCustomBackendSettingsBundle.php
+++ src/HeimseitenContaoCustomBackendSettingsBundle.php
@@ -1,11 +1,16 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Heimseiten\ContaoCustomBackendSettingsBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class HeimseitenContaoCustomBackendSettingsBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
+    }
 }
