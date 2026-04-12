0623e703d3749ef30bb67b0d6bd2379b994cce3d
Niels <info@heimseiten.de>
2026-04-12 21:03:00 +0200

    Change namespace from App to Heimseiten

--- src/InsertTag/ElementGroupCssClassInsertTag.php
+++ src/InsertTag/ElementGroupCssClassInsertTag.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\InsertTag;
+namespace Heimseiten\ContaoCustomBackendSettingsBundle\InsertTag;
 
 use Contao\ContentModel;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsInsertTag;
