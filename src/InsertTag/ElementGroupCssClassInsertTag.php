5e92e9872ef50ca1948d38fa0892e5f0cbfe8254
Niels <info@heimseiten.de>
2026-04-12 20:57:39 +0200

    Add ElementGroupCssClassInsertTag class

---
+++ src/InsertTag/ElementGroupCssClassInsertTag.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\InsertTag;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsInsertTag;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\InsertTag\InsertTagResult;
+use Contao\CoreBundle\InsertTag\OutputType;
+use Contao\CoreBundle\InsertTag\ResolvedInsertTag;
+use Contao\CoreBundle\InsertTag\Resolver\InsertTagResolverNestedResolvedInterface;
+use Contao\StringUtil;
+
+#[AsInsertTag('element_group_cssclass')]
+class ElementGroupCssClassInsertTag implements InsertTagResolverNestedResolvedInterface
+{
+    public function __construct(private readonly ContaoFramework $framework) {}
+
+    public function __invoke(ResolvedInsertTag $insertTag): InsertTagResult
+    {
+        $this->framework->initialize();
+
+        $id = (int) $insertTag->getParameters()->get(0);
+        $model = $this->framework->getAdapter(ContentModel::class)->findByPk($id);
+
+        if (!$model || !$model->cssID) {
+            return new InsertTagResult('', OutputType::text);
+        }
+
+        $cssId = StringUtil::deserialize($model->cssID, true);
+
+        return new InsertTagResult($cssId[1] ?? '', OutputType::text);
+    }
+}
