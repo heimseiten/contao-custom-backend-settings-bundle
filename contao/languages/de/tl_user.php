30740ee9576a87f1f8fcbd090f2da3e834b48c52
Niels <info@heimseiten.de>
2026-04-24 18:12:04 +0200

    Update tl_user.php

--- contao/languages/de/tl_user.php
+++ contao/languages/de/tl_user.php
@@ -15,3 +15,4 @@ $GLOBALS['TL_LANG']['tl_user']['enlargePreviewImagesInFileManager'] = array('Vor
 $GLOBALS['TL_LANG']['tl_user']['disableLinksInPageTreeToFilterTree'] = array('Links zum Filtern im Seitenbaum und in Dateien deaktivieren', 'Deaktiviert die Klickbarkeit der einzelnen Seitennamen um den Seitenbaum zu filtern.');
 $GLOBALS['TL_LANG']['tl_user']['loadBackendSCSS'] = array('eigene SCSS Datei für das Backend laden', 'Lädt die Datei backend.css aus dem Verzeichnis /files/layout/css/.');
 $GLOBALS['TL_LANG']['tl_user']['viewIdAndCssInArticlesAndContentElements'] = array('ID und CSS in Artikeln und Inhaltselementen anzeigen', 'Zeigt die ID und die CSS-Klassen in den Backend-Labels der Artikel und Inhaltselemente an.');
+$GLOBALS['TL_LANG']['tl_user']['cbs_dontViewLayoutSectionsInArticleList.css'] = array('Layout Sections nicht in der Artikelliste anzeigen', '');
