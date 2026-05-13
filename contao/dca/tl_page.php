e00faf38e19e12074c4653ceb5d5c6bd76083bf3
Niels <info@heimseiten.de>
2026-05-13 13:53:42 +0200

    Add cssClass field to tl_page DCA configuration

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -3,3 +3,5 @@
 if ( Contao\Config::get('publishPageOnCreate') ) {
     $GLOBALS['TL_DCA']['tl_page']['fields']['published']['default'] = true;
 }
+$GLOBALS['TL_DCA']['tl_page']['list']['label']['fields'][] = 'cssClass';
+$GLOBALS['TL_DCA']['tl_page']['list']['label']['format'] = '%s <div style="display: none;" class="page_css_wrapper"><span class="page_css">%s</span></div>';
